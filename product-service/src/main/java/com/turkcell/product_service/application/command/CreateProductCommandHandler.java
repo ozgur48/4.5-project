8ea6c47bc5e6dfe87145c72a23144108b199ac0d
Özgür Durak <ozgur.durak@yandex.com>
2025-10-22 13:34:32 +0300

    Add CreateProductCommandHandler, CreateProductMapper, and CreatedProductResponse classes for product creation functionality. Updated ProductEntity (createdAt, updatedAt)

---
+++ product-service/src/main/java/com/turkcell/product_service/application/command/CreateProductCommandHandler.java
@@ -0,0 +1,4 @@
+package com.turkcell.product_service.application.command;
+
+public class CreateProductCommandHandler {
+}
---
+++ product-service/src/main/java/com/turkcell/product_service/application/mapper/CreateProductMapper.java
@@ -0,0 +1,9 @@
+package com.turkcell.product_service.application.mapper;
+
+import com.turkcell.product_service.application.dto.CreatedProductResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreatedProductResponseMapper {
+    public CreatedProductResponse toResponse()
+}
