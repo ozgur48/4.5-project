79af12b61e36f25a8e1d84a2cd63953bb8f929fe
Özgür Durak <ozgur.durak@yandex.com>
2025-10-30 22:48:52 +0300

    Eureka added for order-service. Order-service configurations added

--- order-service/src/main/java/com/turkcell/order_service/controllers/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controllers/OrdersController.java
@@ -1,5 +1,7 @@
 package com.turkcell.order_service.controllers;
 
+import java.util.UUID;
+
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
@@ -8,27 +10,53 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.turkcell.order_service.messaging.outbox.OutboxMessage;
+import com.turkcell.order_service.messaging.outbox.OutboxRepository;
+
 
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrdersController {
     private final StreamBridge streamBridge;
-    public OrdersController(StreamBridge streamBridge) {
+    private final OutboxRepository outboxRepository;
+    private final ObjectMapper objectMapper;
+    
+    public OrdersController(StreamBridge streamBridge, OutboxRepository outboxRepository, ObjectMapper objectMapper) {
         this.streamBridge = streamBridge;
+        this.outboxRepository = outboxRepository;
+        this.objectMapper = objectMapper;
     }
     
     
     @PostMapping()
-    public String createOrder(@RequestBody CreateOrderDto dto) {
+    public String createOrder(@RequestBody CreateOrderDto dto) throws JsonProcessingException {
         OrderCreatedEvent event = new OrderCreatedEvent(dto.productId());
 
-        Message<OrderCreatedEvent> message = MessageBuilder.withPayload(event).build();
-        // kafka eventi fırlat
-        streamBridge.send("orderCreated-out-0",message);
+        OutboxMessage outboxMessage = new OutboxMessage();
+        outboxMessage.setAggregateId(UUID.randomUUID());
+        outboxMessage.setAggregateType("Order");
+        outboxMessage.setEventId(UUID.randomUUID());
+        outboxMessage.setEventType("OrderCreatedEvent");
+        outboxMessage.setPayloadJson(objectMapper.writeValueAsString(event));
+        outboxRepository.save(outboxMessage);
+
         return dto.productId;
     }
     record CreateOrderDto(String productId){  
     }
-    record OrderCreatedEvent(String productId){}
+    public record OrderCreatedEvent(String productId){}
     
 }
+
+/*
+ * Message<OrderCreatedEvent> message = MessageBuilder.withPayload(event).build();
+        try {
+            boolean isSent = streamBridge.send("orderCreated-out-0", message);
+            if (!isSent)
+                System.out.println("Mesaj gönderilemedi");
+        } catch (Exception e) {
+            System.out.println("Mesaj gönderilemedi");
+        }
+ */
--- product-service/src/main/java/com/turkcell/product_service/messaging/consumer/OrderCreatedConsumer.java
+++ product-service/src/main/java/com/turkcell/product_service/messaging/consumer/OrderCreatedConsumer.java
@@ -1,9 +1,9 @@
 package com.turkcell.product_service.messaging.consumer;
 
+import java.util.function.Consumer;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.function.Consumer;
 
 @Configuration
 public class OrderCreatedConsumer {
@@ -11,7 +11,7 @@ public class OrderCreatedConsumer {
     public Consumer<OrderCreatedEvent> orderCreated(){
         return event -> {
             System.out.println("Yeni bir order create edildi");
-            System.out.println(event.productId);
+            System.out.println("Ürün ID:" + event.productId);
         };
     }
     record OrderCreatedEvent(String productId) {
